fb2484d613b648204bdf2c1d510ccbf5a30d21f5
eliezerb7 <eliezer.borde@smiledirectclub.com>
2020-12-18 16:38:43 -0500

    Reformatted Code

--- git_repos.py
+++ git_repos.py
@@ -1,6 +1,5 @@
 import requests
 import os
-import json
 import shutil
 import logging
 
@@ -21,38 +20,41 @@ ch.setFormatter(formatter)
 # add ch to logger
 logger.addHandler(ch)
 
-def get_org_repo(org_name, auth_token):
+
+def get_org_repo(organization, token):
     page_number = 1
     repos = []
 
-    logger.info(f"scanning for organization {org_name}")
+    logger.info(f"scanning for organization {organization}")
     while True:
-        logger.info(f"URL: https://api.github.com/orgs/{org_name}/repos?page={page_number}&per_page=100")
-        response = requests.get(f"https://api.github.com/orgs/{org_name}/repos?page={page_number}&per_page=100",
+        logger.info(f"URL: https://api.github.com/orgs/{organization}/repos?page={page_number}&per_page=100")
+        response = requests.get(f"https://api.github.com/orgs/{organization}/repos?page={page_number}&per_page=100",
                                 headers={
-                                    'Authorization': f'token {auth_token}'
+                                    'Authorization': f'token {token}'
                                 })
         logger.info(f"Response from URL - {response.status_code}")
         repos += response.json()
         page_number += 1
         if len(response.json()) == 0:
             break
-    return {repo['name']: repo['clone_url'] for repo in repos if secret_file_exists(org_name, repo['name'], auth_token) == 404}
+    return {repo['name']: repo['clone_url'] for repo in repos if
+            secret_file_exists(organization, repo['name'], token) == 404}
 
 
-def secret_file_exists(org_name, repo_name, auth_token):
-    logger.info(f"Checking .secrets.baseline file in the repo with URL - https://api.github.com/repos/{org_name}/{repo_name}/contents/.secrets.baseline")
-    response = requests.get(f"https://api.github.com/repos/{org_name}/{repo_name}/contents/.secrets.baseline",
+def secret_file_exists(organization, repo, token):
+    logger.info(f"Checking [.secrets.baseline] file in the repo with URL - "
+                f"https://api.github.com/repos/{organization}/{repo}/contents/.secrets.baseline")
+    response = requests.get(f"https://api.github.com/repos/{organization}/{repo}/contents/.secrets.baseline",
                             headers={
-                                'Authorization': f'token {auth_token}'
+                                'Authorization': f'token {token}'
                             }
                             )
     if response.status_code == 200:
-        logger.info(f".secrets.baseline file exists for repo - {org_name}/{repo_name}")
+        logger.info(f".secrets.baseline file exists for repo - {organization}/{repo}")
     elif response.status_code == 404:
-        logger.info(f".secrets.baseline file doesn't exists for repo - {org_name}/{repo_name}")
+        logger.info(f"[.secrets.baseline] file does not exists for repo - {organization}/{repo}")
     else:
-        logger.error(f"Error Occured: Response status - {response.status_code} for {org_name}/{repo_name}")
+        logger.error(f"Error Occurred: [Response status - {response.status_code} for {organization}/{repo}]")
 
     return response.status_code
 
@@ -60,9 +62,9 @@ def secret_file_exists(org_name, repo_name, auth_token):
 def download_repo(repo_name, repo_url, token):
     repo = repo_url.replace("github.com", f"{token}@github.com")
     # deleting if folder exists
-    dirpath = os.path.join('repos', f'{repo_name}')
-    if os.path.exists(dirpath) and os.path.isdir(dirpath):
-        shutil.rmtree(dirpath)
+    directory = os.path.join('repos', f'{repo_name}')
+    if os.path.exists(directory) and os.path.isdir(directory):
+        shutil.rmtree(directory)
     repo_dir = Repo.clone_from(repo, f'repos/{repo_name}')
     secrets_text = start_scan(repo_dir.working_dir)
     with open(f'{repo_dir.working_dir}/.secrets.baseline', 'w') as secrets_file:
@@ -80,9 +82,9 @@ def git_operations(repo_name, repo_dir, repo_url, token):
     git.add(".secrets.baseline")
     git.commit("-m", "feat: secrets file added")
     git.push("--set-upstream", "origin", branch_name)
-    #
+    # preparing for pull request
     g = Github(token)
-    repo = g.get_repo(str(owner_and_repo))
+    repo = g.get_repo(owner_and_repo)
     body = "Test"
 
     repo.create_pull(
@@ -92,8 +94,9 @@ def git_operations(repo_name, repo_dir, repo_url, token):
         base=base_branch
     )
 
-org_name = ""
-auth_token = ""
+
+org_name = 'CamelotVG'
+auth_token = 'a8b3736c2d09e26bc830a49c634ab358722daac7'
 
 # pprint.pprint(get_org_repo(org_name, auth_token))
 download_repo('lemanga', 'https://github.com/eliezer-borde-globant/lemanga.git', auth_token)
